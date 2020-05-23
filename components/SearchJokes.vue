e0846876951c8f1f2210f1ca96ad96ca38578f03
mrclasse <natjageni@gmail.com>
2020-05-23 10:24:30 -0300

    added all functionalities, style not finished

---
+++ components/SearchJokes.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="search">Search</label>
+    <input placeholder="other side dad joke" type="text" v-model="text" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchJokes",
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("search-text", this.text);
+      this.text = "";
+    }
+  }
+};
+</script>
+
+<style></style>
